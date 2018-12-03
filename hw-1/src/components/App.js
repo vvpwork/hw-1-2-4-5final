a391d2d27b301a4b2cbb313da5af0256eff546a5
vvp <vovvva33@gmail.com>
2018-12-03 13:53:16 +0200

    hw-2

--- hw-1/src/components/App.js
+++ hw-1/src/components/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
-import Menu from './menu/menu';
+import Menu from './menu/Menu';
 import Order from './order/orderTable';
 import SignIn from './formSignIn/SignIn';
 import SignUp from './formSignUp/SignUp';
-import Header from './header/headerApp';
+import Header from './header/HeaderApp';
+import Modal from './modal/Modal'
+
+
+import globalStyle from './global.css'
 
 export default class App extends Component {
+
+    state  = {
+        modalOpen: false,
+    }
+
+    modalOpen=(ev) =>{
+        this.setState({
+            modalOpen: true,
+        })
+    }
+
+    modalClose = (ev)=>{
+        this.setState({
+            modalOpen: false,
+        })
+    }
+
+
   render() {
+      const{modalOpen} = this.state;
     return (
       <div>
         <Header />
+        <button type='button' onClick={this.modalOpen}>OpenModal</button>
+        {modalOpen && <Modal modalClose = {this.modalClose} />}
         {/* <Menu /> */}
         {/* <Order /> */}
         {/* <SignIn /> */}
